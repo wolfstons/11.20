b22a83892c67553131c2760672cd622cae23ab92
wolfstons <paczi.balazs@gmail.com>
2025-11-20 13:46:43 +0100

    valami

--- kiiras.py
+++ kiiras.py
@@ -1,10 +1,13 @@
-import main
+
 
 def kiir(lista):
     print(f'_ _ _ ')
-    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|{lista[0]}|{lista[1]}|{lista[2]}|')
     print(f'|_|_|_|')
-    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|{lista[3]}|{lista[4]}|{lista[5]}|')
     print(f'|_|_|_|')
-    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|{lista[6]}|{lista[7]}|{lista[8]}|')
     print(f'|_|_|_|)')
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+import random
+import kiiras
 vegevan_e=0
-lista=[]
+lista=[" "," "," ",
+       " "," "," ",
+       " "," "," "]
+
+def geplepes():
+    i=0
+    while i<len(lista):
+        pozicio=0
+        gepszam1=random.randint(0,2)
+        gepszam2=random.randint(0,2)
+        if gepszam2>0:
+            pozicio=gepszam1
+            lista[pozicio]="O"
+            return lista
+        if gepszam2==1:
+            pozicio=gepszam1+3
+            lista[pozicio]="O"
+            return lista
+        else:
+            pozicio=gepszam1+6
+            lista[pozicio]="O"
+            return lista
+        i+=1
+
+
+geplepes()
+kiiras.kiir(lista)
