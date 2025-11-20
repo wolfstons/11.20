f7cf2aad81ffedd2ca6777da86be6ce1de41497c
Villo05 <nemethvillo.2005@gmail.com>
2025-11-20 13:13:07 +0100

    kiiras kesz

---
+++ kiiras.py
@@ -0,0 +1,10 @@
+import main
+
+def kiir(lista):
+    print(f'_ _ _ ')
+    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|_|_|_|')
+    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|_|_|_|')
+    print(f'|{lista[1]}|{lista[1]}|{lista[1]}|')
+    print(f'|_|_|_|)')
